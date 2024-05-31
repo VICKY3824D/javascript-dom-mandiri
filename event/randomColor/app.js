f723fb810bb6f705e7ff6c0c95606d2b89af5965
Vidky Adhi Pradana <pradana2766@gmail.com>
2024-05-31 10:45:08 +0700

    Menggenerate random color dengan Event DOM

---
+++ event/randomColor/app.js
@@ -0,0 +1,21 @@
+const button = document.querySelector('button');
+const h1 = document.querySelector('h1');
+
+// mencari warna random 
+function generateRandomColor(){
+    const red = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
+
+    return `rgb(${red}, ${green}, ${blue})`
+}
+
+// menggenerate warna random ketika button di klik
+button.addEventListener('click', () => {
+    const newGenerator = generateRandomColor();
+    document.body.style.backgroundColor = newGenerator;
+    h1.innerText = newGenerator;
+    console.log(newGenerator);
+});
+
+
