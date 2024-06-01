8659bc0d401f4e8229cf50de82d1ebf86f59befe
Vidky Adhi Pradana <pradana2766@gmail.com>
2024-06-01 14:49:29 +0700

    CRUD sederhana dengan Event Listener

--- event/preventDefault/app.js
+++ event/preventDefault/app.js
@@ -1,11 +1,28 @@
 const form = document.querySelector('#form');
+const input = document.querySelector('input');
+const list = document.querySelector("#notes");
 
-// form.addEventListener('submit', function (execute) {
-//     execute.preventDefault();
-//     console.log('Submitted form');
-// });
+// menambah list menggunakan input  keyboard
+form.addEventListener('submit', function (execute) {
+    execute.preventDefault();
+    const newLi = document.createElement('li');
+    newLi.innerText = input.value;
 
-document.querySelector('a').addEventListener('click', (e) => {
-    e.preventDefault();
-    console.log("Harusnya ke google.com");
+    list.append(newLi);
+    input.value = '';
+
 });
+
+//menghapus list satu per satu dari atas (stack)
+const del = document.querySelector("#del");
+del.addEventListener('click', () => {
+    const currentLi1 = document.querySelector('li');
+    currentLi1.remove();
+});
+
+//menghapus semua list (ada bug)
+// const dela = document.querySelector("#dela");
+// dela.addEventListener('click', () => {
+//     const currentLi2 = document.querySelector('#notes');
+//     currentLi2.remove();
+// });
