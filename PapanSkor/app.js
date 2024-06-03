1646d29f483b7904bee2507b759cfd8a7addc889
Vidky Adhi Pradana <pradana2766@gmail.com>
2024-06-03 08:59:19 +0700

    Menambahkan Bootstrap

--- PapanSkor/app.js
+++ PapanSkor/app.js
@@ -1,15 +1,16 @@
 const p1Button = document.querySelector('#p1-button');
 const p2Button = document.querySelector('#p2-button');
-const reset = document.querySelector("#reset");
+const btnReset = document.querySelector('#reset');
+const SelectWinPoint = document.querySelector('#masukan');
 
 const p1Display = document.querySelector('#p1-display');
 const p2Display = document.querySelector('#p2-display');
 
-let maxPoint = 10;
+let maxPoint = 30;
 let isMax = false;
 
 let P1Score = 0;
-p1Button.addEventListener('click', function () {
+p1Button.addEventListener('click', () => {
     if (!isMax) {
         P1Score += 1; 
         if (P1Score === maxPoint) {
@@ -20,7 +21,7 @@ p1Button.addEventListener('click', function () {
 });
 
 let P2Score = 0;
-p2Button.addEventListener('click', function () {
+p2Button.addEventListener('click',() => {
     if (!isMax) {
         P2Score += 1;
         if (P2Score === maxPoint) {
@@ -31,7 +32,16 @@ p2Button.addEventListener('click', function () {
     
 });
 
-reset.addEventListener('click',() => {
-    
+btnReset.addEventListener('click', () => {
+    isMax = false;
+    P1Score = 0;
+    P2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+});
+
+SelectWinPoint.addEventListener('change', () => {
+    maxPoint = parseInt(this.value);
+    reset();
 });
 
